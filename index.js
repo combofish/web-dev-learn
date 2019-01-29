93c54dff5a3ec9920891b46012096d17adc01311
combofish <18233092052@139.com>
2019-01-29 09:24:54 +0800

    some little

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const http = require('http'),
       credentials = require('./credentials.js'),
       fs = require('fs'),
       routes = require('./routes.js'),
+      rest = require('connect-rest'),
       stuff = require('./lib/stuff.js')({options:'my choice'});
 //    getIp = require("./lib/get-ip.js"); 
 
@@ -226,53 +227,75 @@ var Attraction = require('./models/attraction.js');
 //     approved:true,
 // }).save();
 
-app.get('/api/attractions',function(req,res){
-    Attraction.find({approved:true}, function(err,attractions){
-	if(err) return res.send(500, 'Error occured: database error.');
-	res.json(attractions.map(function(a){
-	    return {
-		name:a.name,
-		id:a.id,
-		description:a.description,
-		location:a.location,
-	    }
-	}));
-    });
-});
+// app.get('/api/attractions',function(req,res){
+//     Attraction.find({approved:true}, function(err,attractions){
+// 	if(err) return res.send(500, 'Error occured: database error.');
+// 	res.json(attractions.map(function(a){
+// 	    return {
+// 		name:a.name,
+// 		id:a.id,
+// 		description:a.description,
+// 		location:a.location,
+// 	    }
+// 	}));
+//     });
+// });
 
 app.get('/api/attraction',function(req,res){
     res.render('attraction')
 });
 
-app.post('/api/attraction', function(req,res){
-    var a = new Attraction({
-	name:req.body.name,
-	description:req.body.description,
-	location:{ lat: req.body.lat, lng: req.body.lng },
-	history:{ event:'created',
-		  email: req.body.email,
-		  date: new Date(),
-		},
-	approved: false,
-    });
-    a.save(function(err,a){
-	if(err) return res.send(500,'Error occured: database error.');
-	res.json({ id: a.id});
-    })
-});
+// app.post('/api/attraction', function(req,res){
+//     var a = new Attraction({
+// 	name:req.body.name,
+// 	description:req.body.description,
+// 	location:{ lat: req.body.lat, lng: req.body.lng },
+// 	history:{ event:'created',
+// 		  email: req.body.email,
+// 		  date: new Date(),
+// 		},
+// 	approved: false,
+//     });
+//     a.save(function(err,a){
+// 	if(err) return res.send(500,'Error occured: database error.');
+// 	res.json({ id: a.id});
+//     })
+// });
 
-app.get('/api/attraction/:id',function(req,res){
+// app.get('/api/attraction/:id',function(req,res){
 
-    Attraction.findById(req.params.id,function(err,a){
-	if(err) return res.send(500,'Error occured, database error.');
-	res.json({
-	    name:a.name,
-	    id:a.id,
-	    description:a.description,
-	    location:a.location,
-	});
-    });
-})
+//     Attraction.findById(req.params.id,function(err,a){
+// 	if(err) return res.send(500,'Error occured, database error.');
+// 	res.json({
+// 	    name:a.name,
+// 	    id:a.id,
+// 	    description:a.description,
+// 	    location:a.location,
+// 	});
+//     });
+// })
+
+var apiOpts = {
+    context : '/api',
+};
+
+//app.use(rest.create(apiOpts))
+
+// rest.get('/attractions',function(req,context,cb){
+//     Attraction.find({approved:true}, function(err,attractions){
+// 	if(err) return cb({ error: 'Error occured: database error.' });
+// 	cb(null,attractions.map(function(a){
+// 	    return {
+// 		name:a.name,
+// 		id:a.id,
+// 		description:a.description,
+// 		location:a.location,
+// 	    }
+// 	}));
+//     });
+// });
+
+ 
 app.use(function(req,res){
     // res.type('text/plain');
     res.status(404);
